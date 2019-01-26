6a2d98a53d3ebe928e7729e6aadb096d0091f248
hs <hs@hsui-MacBook-Air.local>
2019-01-26 13:29:34 +0900

    CORS 처리 모듈 추가

--- route/router.go
+++ route/router.go
@@ -1,12 +1,24 @@
 package route
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"go-api/controllers"
+	"time"
 )
 
 func Route()  {
 	router := gin.Default()
+
+	router.Use(cors.New(cors.Config{
+		AllowAllOrigins: true,
+		//AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"PUT", "POST", "DELETE", "GET"},
+		AllowHeaders:     []string{"Origin"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge: 12 * time.Hour,
+	}))
 
 	member := router.Group("/api/v1/member")
 	{
